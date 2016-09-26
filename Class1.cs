a350e6b9949646750c3dd0fc18e3bb1a9e6adac3
Evert Kramer <evertk@deltainstruments.com>
2016-09-26 16:56:08 +0200

    Added nuget package necessary files.

--- Class1.cs
+++ Class1.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace FabrikamLibrary
 {
-    public class Class1
-    {
-    }
+	public class Shout
+	{
+		public string AwesomeString()
+		{
+			return "Awesome!";
+		}
+	}
 }
